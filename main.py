218d23b4c20f270dde500b4800afb9ed926aa323
MannasehM <codingMann007@gmail.com>
2025-06-03 13:46:29 -0400

    Added `!department` feature

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import discord
 from discord.ext import commands
-# print(discord.__file__)
-# print(dir(discord))
-# print(hasattr(discord, 'Bot'))
+
 import os
 from dotenv import load_dotenv
-from met_api import get_random_artwork
+from met_api import get_random_artwork, get_departments, get_artworks_by_department
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -25,7 +23,7 @@ async def on_ready():
 
 @bot.command(name="art", description="Get a random artwork from the MET Museum")
 async def art(ctx):
-    max_retries = 5
+    max_retries = 3
     for attempt in range(max_retries):
         art = get_random_artwork()
         if art["image"]:
@@ -39,6 +37,64 @@ async def art(ctx):
             return
     # If no image found after retries:
     await ctx.send("Sorry, I couldn’t find any artwork images after several tries. Please try again later!")
+
+# @bot.command(name="search", help="Search for an artwork by keyword")
+# async def search(ctx, *, query):
+#     art = search_artwork(query)
+    
+#     if not art or not art["image"]:
+#         await ctx.send("No artwork found for that keyword!")
+#         return
+    
+#     embed = discord.Embed(
+#         title=art["title"],
+#         url=art["url"],
+#         description=f"Artist: {art['artist']}\nDate: {art['date']}"
+#     )
+#     embed.set_image(url=art["image"])
+#     await ctx.send(embed=embed)
+
+@bot.command(name="department-id", help="Get department IDs for each art department")
+async def find_department_ids(ctx): 
+    departments_data = get_departments()
+    
+    embed = discord.Embed(
+        title="List of Department and Department IDs",
+        description=format_department_ids(departments_data)
+    )
+
+    await ctx.send(embed=embed)
+
+@bot.command(name="department", help="Get a random artwork from a department from an department ID")
+async def department(ctx, *, department_id: int):
+    max_retries = 3
+    for attempt in range(max_retries):
+        art = get_artworks_by_department(department_id)
+        
+        if not art: 
+            await ctx.send("No artworks found for that department.")
+            return
+        
+        if art["image"]:
+            embed = discord.Embed(
+                title=art["title"], 
+                url=art["url"], 
+                description=f"Artist: {art['artist']}\nDate: {art['date']}"
+            )
+            embed.set_image(url=art["image"])
+            await ctx.send(embed=embed)
+            return
+    # If no image found after retries:
+    await ctx.send("Sorry, I couldn’t find any artwork images after several tries. Please try again later!")
+
+def format_department_ids(departments_data):
+    result = ""
+    for dept in departments_data: 
+        id = dept["departmentId"]
+        name = dept["displayName"]
+        result += f"{name} - {id}\n"
+
+    return result[:-1]
 
 # Starts Discord Bot, and stays running for event/command listening
 bot.run(TOKEN)
--- met_api.py
+++ met_api.py
@@ -24,3 +24,48 @@ def get_random_artwork():
         "image": art_data.get("primaryImageSmall"),
         "url": art_data.get("objectURL"),
     }
+
+# def search_artwork(query):
+#     response = requests.get(f"{BASE_URL}/search", params={"q": query})
+#     data = response.json()
+    
+#     if not data["objectIDs"]:
+#         return None
+    
+#     random_art_id = random.choice(data["objectIDs"])
+#     art_data = requests.get(f"{BASE_URL}/objects/{random_art_id}").json()
+    
+#     return {
+#         "title": art_data["title"],
+#         "artist": art_data.get("artistDisplayName", "Unknown"),
+#         "date": art_data.get("objectDate", "Unknown"),
+#         "image": art_data.get("primaryImage", None),
+#         "url": art_data["objectURL"]
+#     }
+
+def get_artworks_by_department(department_id: int):
+    response = requests.get(f"{BASE_URL}/objects", params={"departmentIds": department_id}) # FIX THIS!!!
+    data = response.json()
+    
+    # if there are no objects with the department above, return None
+    if not data["objectIDs"]:
+        return None
+    
+    random_art_id = random.choice(data["objectIDs"])
+    art_data = requests.get(f"{BASE_URL}/objects/{random_art_id}").json()
+    
+    return {
+        "title": art_data["title"],
+        "artist": art_data.get("artistDisplayName", "Unknown"),
+        "date": art_data.get("objectDate", "Unknown"),
+        "image": art_data.get("primaryImage", None),
+        "url": art_data["objectURL"]
+    }
+
+def get_departments(): 
+    response = requests.get(f"{BASE_URL}/departments")
+    data = response.json()
+
+    # List of Dictionaries that each have an departmentId and a displayName
+    departments_data = data["departments"]
+    return departments_data
